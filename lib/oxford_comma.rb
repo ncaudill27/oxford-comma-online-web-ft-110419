6a26545adb0ff0b61c008f6a7d48fd9aa4a6bc16
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-07 00:14:47 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  case array.length
+  when 1
+    return array.join(", ")
+  when 2
+    array.insert(-2, "and")
+    array.join(" ")
+  else
+    cap = array.pop
+    string = array.join(", ")
+    string << ", and " + cap
+  end
 end
